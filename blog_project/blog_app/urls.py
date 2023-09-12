fedd4500e1aab662cd05b2cb37040538c1b1271c
Oneiric0102 <ndmjlove2@gmail.com>
2023-09-12 17:19:38 +0900

    #1 html files edit

--- blog_project/blog_app/urls.py
+++ blog_project/blog_app/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path,include
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.admin_page, name = 'admins'),
-    path('board/', views.board_page , name = 'board'),
-    path('client/', views.client_page, name = 'client'),
-    path('write/', views.write_page, name = 'write'),
+    path("<string:owner_id>/admin", views.admin_page, name="admins"),
+    path("<string:owner_id>/post/<int:post_id>", views.post_page, name="post"),
+    path("<string:owner_id>", views.client_page, name="client"),
+    path("write/", views.write_page, name="write"),
+    path("edit/", views.edit_page, name="edit"),
+    path("delete/", views.delete_page, name="delete"),
 ]
