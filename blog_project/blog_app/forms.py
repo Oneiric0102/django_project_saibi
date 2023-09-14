59b0c706aebe71a6bb28ce21752d143477397d23
ansoyun12 <dksthdbs43@gmail.com>
2023-09-14 17:15:34 +0900

    feat: ckeditor 라이브러리 설치

--- blog_project/blog_app/forms.py
+++ blog_project/blog_app/forms.py
@@ -1,14 +1,31 @@
 from django import forms
 from .models import Board
+from ckeditor.widgets import CKEditorWidget
+
 
 class TempPostForm(forms.ModelForm):
-    is_draft = forms.BooleanField(required=False, initial=True, widget=forms.CheckboxInput(attrs={'class': 'checkbox'}))
+    is_draft = forms.BooleanField(
+        required=False, initial=True, widget=forms.CheckboxInput(attrs={"class": "checkbox"})
+    )
 
     class Meta:
         model = Board
-        fields = ['title', 'content', 'is_draft']
+        fields = ["title", "content", "is_draft"]
+
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(
+        required=True,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "style": "border: 1px solid #dbdbdb; width: 735px; height:30px;",
+                "placeholder": " 제목",
+            }
+        ),
+    )
+    content = forms.CharField(widget=CKEditorWidget(config_name="default"), label="")
+
     class Meta:
         model = Board
-        fields = ['title', 'content']
+        fields = ["title", "content"]
--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,5 +26,5 @@ urlpatterns = [
     ############################################################################## 09/12추가3
     path('accounts/',include('allauth.urls')),
     ############################################################################## 09/12추가3
-    
-]
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
